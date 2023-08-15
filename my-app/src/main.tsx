07a5cf37b56b57818111b6a9f459c1ded7589a11
sowarney <sowarney@sowarneyui-MacBook-Pro.local>
2023-08-15 21:23:40 +0900

    setting save

---
+++ my-app/src/main.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+
+function App(){
+    return <p>!!?!??!?</p>
+}
+
+const element = document.getElementById('root')
+if(element){
+const root = ReactDOM.createRoot(element)
+root.render(<App/>)
+}
