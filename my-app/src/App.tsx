1aaae3f90ccd8f1ea6e988ca8d26b3f09cdc3744
sowarney <sowarney@sowarneyui-MacBook-Pro.local>
2023-08-20 21:31:50 +0900

    save

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -4,11 +4,12 @@ export default function App(){
     const [count, setCount] = useState(0)
 
     const handleClick = () => {
-        setCount(count + 1)
+        setCount( count + 1 )
     }
 
     return (
         <div>
+        <img src="/images/parcel.png" alt="TestImg"/>
         <p>Hello, world!</p>
         <p>Count: {count}</p>
         <button type="button" onClick={handleClick}>클릭!</button>
